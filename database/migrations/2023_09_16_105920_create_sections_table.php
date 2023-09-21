ddb7fa1e923d84311484e1bb0748229500543e1d
developerjannatmim <mim266758@gmail.com>
2023-09-21 09:56:04 +0600

    add

--- database/migrations/2023_09_16_105920_create_sections_table.php
+++ database/migrations/2023_09_16_105920_create_sections_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('sections', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('class_id');
+            $table->foreign('class_id')->references('id')->on('classes');
             $table->unsignedBigInteger('school_id');
             $table->foreign('school_id')->references('id')->on('schools');
             $table->timestamps();
