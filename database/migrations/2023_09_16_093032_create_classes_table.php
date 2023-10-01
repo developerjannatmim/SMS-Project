2745c88441a7c2ba414852048875f5b97d407106
developerjannatmim <mim266758@gmail.com>
2023-10-01 17:03:41 +0600

    fixed migrations

--- database/migrations/2023_09_16_093032_create_classes_table.php
+++ database/migrations/2023_09_16_093032_create_classes_table.php
@@ -16,6 +16,8 @@ return new class extends Migration
         Schema::create('classes', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('section_id');
+            $table->foreign('section_id')->references('id')->on('sections');
             $table->unsignedBigInteger('school_id');
             $table->foreign('school_id')->references('id')->on('schools');
             $table->timestamps();
